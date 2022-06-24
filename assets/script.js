49046cfc7191899a34b712fb532be38490e84c4f
Chrisbick94 <101513319+Chrisbick94@users.noreply.github.com>
2022-06-23 21:03:04 -0400

    Game
    Added a Block.. working on making more blocks to break

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20
+
+
+// Blocks
+class Block {
+    constructor(xAxis, yAxis) {
+      this.bottomLeft = [xAxis, yAxis]
+      this.bottomRight = [xAxis + blockWidth, yAxis]
+      this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+      this.topLeft = [xAxis, yAxis + blockHeight]
+    }
+  }
+  
+
+const blocks = [
+    new Block(10,270)
+]
+
+
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+        grid.appendChild(block)
+        console.log(blocks[i].bottomLeft)
+    }
+}
+addBlocks()
+
